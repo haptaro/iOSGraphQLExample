ff869e92695dacc202c03840a4969203d88eb95c
Kotaro Fukuo <haptaro.dev@gmail.com>
2022-12-02 21:43:15 -0800

    Initial Commit

---
+++ iOSGraphQLExample/iOSGraphQLExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSGraphQLExampleApp.swift
+//  iOSGraphQLExample
+//
+//  Created by Kotaro Fukuo on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct iOSGraphQLExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
